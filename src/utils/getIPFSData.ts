fc7fac867a61d2b99dd332cdc7eeb2487895fc42
Csaba Bolyos <bladeszasza@gmail.com>
2022-08-19 11:25:51 +0000

    updated to use reality crisis dedicated gateway

--- src/utils/getIPFSData.ts
+++ src/utils/getIPFSData.ts
@@ -6,13 +6,9 @@ import axios from "axios";
  * @param hash IPFS Hash
  * @returns byte array stored inside reponse.data
  */
-//`https://gateway.pinata.cloud/ipfs/${hash}`
-//`https://2eff.lukso.dev/ipfs/${hash}`
-//`https://api.ipfsbrowser.com/ipfs/download.php?hash=${hash}`
-// https://b8bskateboards.mypinata.cloud/ipfs/QmbUivWqkpEk7m2wPHjQ8YukdYZ1Kvfyko3Aw7QAfHQnf4
 
 export const getData = async (hash: string) => {
-    return axios.get(`https://b8bskateboards.mypinata.cloud/ipfs/${hash}`, {responseType: 'arraybuffer', responseEncoding: 'binary', timeout: 0})
+    return axios.get(`https://realitycrisis.mypinata.cloud/ipfs/${hash}`, {responseType: 'arraybuffer', responseEncoding: 'binary', timeout: 0})
     .then((response: any) => {
         //the response.data holds the byte array
         return response.data;
